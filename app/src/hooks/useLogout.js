bbee0304f285d70ed1ae8dbeb465ee5bb7a1bdb5
Shashank <shashank08090@gmail.com>
2022-12-10 10:38:36 +0530

    localstorage_false_on_logout

--- app/src/hooks/useLogout.js
+++ app/src/hooks/useLogout.js
@@ -1,13 +1,18 @@
 import React, { useContext } from "react";
 import axios from "../api/axios";
 import useAuth from "./useAuth";
-
+import {
+  localstoragetrue,
+  localstoragefalse,
+} from "../localstoragereducer/localstoragereducer";
+import { useDispatch, useSelector } from "react-redux";
 const useLogout = () => {
   const { setAuth, auth } = useAuth();
-
+  const dispatch = useDispatch();
   const logout = async () => {
     setAuth({});
     localStorage.clear();
+    dispatch(localstoragefalse());
     try {
       await axios.get("/auth/logout");
       console.log(auth);
